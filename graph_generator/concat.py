e1519a4de4e51926cf696bd601182bd08ccd1a80
Sixian <lisixian579@gmail.com>
2020-03-29 15:58:41 -0400

    close #9

--- graph_generator/concat.py
+++ graph_generator/concat.py
@@ -8,10 +8,10 @@ A helper script to concatenate all courses to a single all_courses file
 CURRENT_DIR = Path(__file__).parent
 DATA_DIR = (CURRENT_DIR.parent / 'course_data').resolve()
 
-output_name = 'all_courses.jl'
-with open((CURRENT_DIR / output_name).resolve(), 'wb') as outfile:
-    for filename in DATA_DIR.glob('*.jl'):
-        if filename == output_name:
-            continue
-        with open(filename, 'rb') as readfile:
-            shutil.copyfileobj(readfile, outfile)
+def concat(output_name):
+    with open((CURRENT_DIR / output_name).resolve(), 'wb') as outfile:
+        for filename in DATA_DIR.glob('*.jl'):
+            if filename == output_name:
+                continue
+            with open(filename, 'rb') as readfile:
+                shutil.copyfileobj(readfile, outfile)
--- update.py
+++ update.py
@@ -2,12 +2,15 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from courseSpider.spiders import course_spider
+from graph_generator.concat import concat
 
 
 process = CrawlerProcess(get_project_settings())
 process.crawl("courses")
+# the script will block here until the crawling is finished
 process.start()
-
+# After all courses are updated, produce the all_courses.jl file
+concat("all_courses.jl")
 
 
 
